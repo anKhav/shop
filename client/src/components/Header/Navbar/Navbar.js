e5966673b4a2c52d9fb037ffe59f4c280f45f065
anKhav <khavaldzhi.a@gmail.com>
2022-10-10 20:04:42 +0200

    for deploy

--- client/src/components/Header/Navbar/Navbar.js
+++ client/src/components/Header/Navbar/Navbar.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
+import './navbar.scss'
 
 const Navbar = () => {
     return (
-        <nav>
+        <nav className="navbar">
             <NavLink className='nav__link' to='/'>Home</NavLink>
             <NavLink className='nav__link' to='/about'>About</NavLink>
             <NavLink className='nav__link' to='/contact'>Contact Us</NavLink>
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,52 +1,3 @@
-// const ApiError = require('../error/ApiError')
-// const bcrypt = require('bcrypt')
-// const jwt = require('jsonwebtoken')
-// const {User} = require('../models/models')
-//
-// const generateJwt = (id, email, role) => {
-//     return jwt.sign(
-//         {id, email, role},
-//         process.env.SECRET_KEY,
-//         {expiresIn:'24h'}
-//     )
-// }
-//
-// class UserController {
-//     async registration(req, res, next) {
-//         const {email, password, role} = req.body
-//         if (!email || !password) {
-//             return next(ApiError.badRequest('No email or password'))
-//         }
-//         const candidate = await User.findOne({where: {email}})
-//         if (candidate) {
-//             return next(ApiError.badRequest('User already exists'))
-//         }
-//         const hashPassword = await bcrypt.hash(password, 5)
-//         const user = await User.create({email, role, password: hashPassword})
-//         // const basket = await Basket.create({userId: user.id})
-//         const token = generateJwt(user.id, user.email, user.role)
-//         res.json({token})
-//     }
-//     async login(req, res, next) {
-//         const {email, password} = req.body
-//         const user = await User.findOne({where:{email}})
-//         if(!user){
-//             return next(ApiError.badRequest('User doesnt found'))
-//         }
-//         let comparePassword = bcrypt.compareSync(password, user.password)
-//         if(!comparePassword){
-//             return next(ApiError.badRequest('Incorrect password'))
-//         }
-//         const token = generateJwt(user.id, user.email, user.role)
-//         return res.json({token})
-//     }
-//     async check(req, res) {
-//         const token = generateJwt(req.user.id, req.user.email, req.user.role)
-//         return res.json({token})
-//     }
-// }
-// module.exports = new  UserController()
-
 const userService = require('../service/userService')
 const orderService = require('../service/orderService')
 const ApiError = require('../error/ApiError')
@@ -95,7 +46,7 @@ class UserController {
         try {
             const activationLink = req.params.link
             await userService.activate(activationLink)
-            return res.redirect('http://google.com/')
+            return res.redirect('http://localhost:3000/')
         } catch (e){
             return next(ApiError.badRequest(e.message))
         }
